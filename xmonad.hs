ed3b5a46558950c38b52adf0a0f9ba46d6cf2c52
Jason Graham <jason@the-graham.com>
2012-01-06 22:10:47 -0800

    Eliminate cruft

--- xmonad.hs
+++ xmonad.hs
@@ -5,25 +5,18 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
-myManageHook = composeAll
-  [ className =? "Gimp" --> doFloat
-  , className =? "Vncviewer" --> doFloat
-  ]
-
 main = do
   xmproc <- spawnPipe "xmobar"
   xmonad $ defaultConfig
     { terminal = "urxvt"
     , modMask = mod4Mask -- rebind to windows key
-    , manageHook = manageDocks <+> myManageHook
-                    <+> manageHook defaultConfig
+    , manageHook = manageDocks <+> manageHook defaultConfig
     , layoutHook = avoidStruts $ layoutHook defaultConfig
     , logHook = dynamicLogWithPP xmobarPP
     		{ ppOutput = hPutStrLn xmproc
     		, ppTitle = xmobarColor "green" "" . shorten 50
     		}
     } `additionalKeys`
-    [ ((mod4Mask, xK_z), spawn "xscreensaver-command -lock")
     , ((mod4Mask, xK_Print), spawn "sleep 0.5; scrot -s")
     , ((0, xK_Print), spawn "scrot")
     ]
