97e9ffa6c79fc3f52409641ed21dc4777b84c5e8
Jason Graham <jason@the-graham.com>
2012-05-20 16:25:17 -0700

    Add a setup file

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Quick script to setup the xmonad config by linking
+# $HOME/.xmonad to this repo.
+
+# Get the directory name of the setup script
+DIR="$( cd "$(dirname "$0")" && pwd)"
+
+# make sure that we're not already in $HOME/.xmonad
+if [ $DIR == $HOME/.xmonad ] ; then
+    echo "This script is intended to link $HOME/.xmonad to $DIR."
+    echo "If those are the same, there's nothing to do."
+    exit 1
+fi
+
+rm -rf $HOME/.xmonad $HOME/.xmobarrc
+ln -s $DIR $HOME/.xmonad
+ln -s $HOME/.xmonad/xmobarrc $HOME/.xmobarrc
+
